6837e2e3ab4e2bf9d357203efc0d62aa88d97c8c
Alessandro Cappellozza <alessandro.cappellozza@gmail.com>
2012-06-11 18:43:14 +0200

    Source Tree Changes
    New Structure
    New Name HTTPING

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // Le informazioni generali relative a un assembly sono controllate dal seguente 
 // set di attributi. Per modificare le informazioni associate a un assembly
 // occorre quindi modificare i valori di questi attributi.
-[assembly: AssemblyTitle("htping")]
+[assembly: AssemblyTitle("httping")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
